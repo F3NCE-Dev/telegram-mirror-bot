61a2a493ad106f1358a01c35b5642dd18026329d
F3NCENMAN <thefrancandman@gmail.com>
2026-01-15 13:12:24 +0200

    bot_send fix bot-dev06

--- handlers/routers/BOT_SEND.py
+++ handlers/routers/BOT_SEND.py
@@ -8,12 +8,12 @@ router = Router()
 @router.message()
 async def send_message_byID(message: Message):
     user_id = message.chat.id
-    for branch_name, input_id, output_id in Get_Branch_DB(user_id):
-        if (message.chat.id == output_id):
+    for branch_name, input_id, output_id, status in Get_Branch_DB(user_id):
+        if (message.chat.id == output_id) and status:
             if message.content_type == "text":
                 Bot_message = message.text
 
-                if Get_Addition_DB(user_id):
+                if Is_There_Addtion(user_id):
                     test = Get_Addition_DB(user_id)
                     Bot_message += f"\n\n{test}"
 
